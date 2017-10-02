2b1bbf2abcd155e296657f9bbeded206b2f501e5
djonesdev <31727436+djonesdev@users.noreply.github.com>
2017-10-02 22:04:57 +0100

    Add files via upload

---
+++ Country_rails/app/controllers/countries_controller.rb
@@ -0,0 +1,44 @@
+class CountriesController < ApplicationController
+
+ def index
+      @countries = Country.all
+  end
+
+  def show
+    @countries = Country.find(params[:id])
+  end
+
+  def edit
+    @countries = Country.find(params[:id])
+  end
+
+  def create
+    new_country = Country.new
+    new_country.name = params[:name]
+    new_country.population = params[:population]
+    new_country.language = params[:language]
+    new_country.president = params[:president]
+  end
+
+  def new
+  end
+
+  def update
+    @country = Country.find(params[:id])
+    @country.name = params[:name]
+    @country.population = params[:population]
+    @country.language = params[:language]
+    @country.president = params[:president]
+
+    @country.save
+
+    redirect_to "/country"
+  end
+
+  def destroy
+    Country.find(params[:id]).destroy
+
+    redirect_to "/country"
+  end
+end
+
---
+++ Country_rails/app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+class StaticController < ApplicationController
+
+	def home 
+		render plain: "welcome to my site"
+	end 
+
+
+	
+end
